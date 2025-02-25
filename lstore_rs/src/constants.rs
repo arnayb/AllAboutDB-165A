292810b045be17590e0224b519fa657ac28e1c04
Jason Y <ysyoo@ucdavis.edu>
2025-02-24 17:59:23 -0800

    existing primary key insertion

--- lstore_rs/src/constants.rs
+++
@@ -1,5 +0,0 @@
-pub const RID_COLUMN: i64 = -2;
-pub const INDIRECTION_COLUMN: i64 = -3;
-pub const TIMESTAMP_COLUMN: i64 = -4;
-pub const SCHEMA_ENCODING_COLUMN: i64 = -5;
-pub const PAGE_SIZE: i64 = 4096 * 1024; // 4KB pages
