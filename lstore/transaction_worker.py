144b81a4921e637897b469f3bb844b722cb158e9
Jason Y <ysyoo@ucdavis.edu>
2025-03-14 16:23:06 -0700

    workers

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,27 +1,19 @@
 from lstore.table import Table, Record
 from lstore.index import Index
-from threading import Thread, Lock
-from config import NUM_THREADS
+from threading import Thread
 
 import queue
 
-class TransactionWorker:
+class TransactionWorker(Thread):
 
     """
     # Creates a transaction worker object.
     """
     def __init__(self, transactions = []):
-        # self.stats = []
-        self.threads = []
-        self.lock = Lock()
-
-        self.transactions = queue.Queue()
-        for transaction in transactions:
-            self.transactions.put(transaction)
-
+        super().__init__()
+        self.stats = []
+        self.transactions = transactions
         self.result = 0
-        pass
-
     
     """
     Appends t to transactions
@@ -35,26 +27,19 @@ class TransactionWorker:
     """
     def run(self):
         # here you need to create a thread and call __run
-        for _ in range(NUM_THREADS):
-            thread = Thread(target= self.__run)
-            thread.start()
-            self.threads.append(thread)
-    
+        self.thread = Thread(target=self.__run)
+        self.thread.start()
 
     """
     Waits for the worker to finish
     """
     def join(self):
-        pass
-
+        self.thread.join()
 
     def __run(self):
-        while True:
-            transaction = self.transactions.get()
-            with self.lock:
-                # self.stats.append(transaction.run()) - it was given in the skeleton code but idk if we need stats
-                self.result += transaction.run()
-                
+        for transaction in self.transactions:
+              self.stats.append(transaction.run())
+
         # stores the number of transactions that committed
-        # self.result = len(list(filter(lambda x: x, self.stats)))
+        self.result = len(list(filter(lambda x: x, self.stats)))
 
